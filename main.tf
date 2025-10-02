c5aee877de15575276c3694cf598a0e1e8aee695
Adam Sobkowicz <adamsobkowicz0@gmail.com>
2025-10-02 21:32:48 +0200

    test4

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # Twój jeden stack
 resource "grafana_cloud_stack" "my_stack" {
-  name               = "demo-stack"
-  slug               = "demo-stack"
+  name               = "adamssstf2"
+  slug               = "adamssstf2"
   region_slug        = "eu"
   delete_protection  = true
 }
