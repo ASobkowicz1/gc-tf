ea255a0a60ccee5bdb85e9fbf220ba1e4ae47a1f
Adam Sobkowicz <adamsobkowicz0@gmail.com>
2025-10-02 21:36:09 +0200

    test5

--- main.tf
+++ main.tf
@@ -1,13 +1,38 @@
-# Twój jeden stack
 resource "grafana_cloud_stack" "my_stack" {
-  name               = "adamssstf2"
-  slug               = "adamssstf2"
-  region_slug        = "eu"
-  delete_protection  = true
+  provider              = grafana.cloud
+  name                  = "adamssstf2"
+  slug                  = "adamssstf2"
+  region_slug           = "eu"
+  delete_protection     = true
 }
 
-# Przykładowy folder w stacku
+// Step 2: Create a service account and key for the stack
+resource "grafana_cloud_stack_service_account" "cloud_sa" {
+  provider   = grafana.cloud
+  stack_slug = grafana_cloud_stack.my_stack.slug
+
+  name        = "sa_tf2"
+  role        = "Admin"
+  is_disabled = false
+}
+
+resource "grafana_cloud_stack_service_account_token" "cloud_sa" {
+  provider   = grafana.cloud
+  stack_slug = grafana_cloud_stack.my_stack.slug
+
+  name               = "terraform serviceaccount key"
+  service_account_id = grafana_cloud_stack_service_account.cloud_sa.id
+}
+
+// Step 3: Create resources within the stack
+provider "grafana" {
+  alias = "my_stack"
+
+  url  = grafana_cloud_stack.my_stack.url
+  auth = grafana_cloud_stack_service_account_token.cloud_sa.key
+}
 resource "grafana_folder" "my_folder" {
-  title    = "Test Folder"
-  depends_on = [grafana_cloud_stack.my_stack]
+  provider = grafana.my_stack
+
+  title = "Test Folder"
 }
