a3286181d9ebe2d17bcd68ee7e7dc9f30eb3eee8
Adam Sobkowicz <adamsobkowicz0@gmail.com>
2025-10-10 15:24:00 +0200

    5

--- environments/dev/access_control.tf
+++ environments/dev/access_control.tf
@@ -2,7 +2,7 @@ resource "grafana_team" "team_x" {
   provider = grafana.stack
   name     = "Team X"
 }
-
+/*
 resource "grafana_data_source_permission" "prometheus_for_viewers" {
   provider       = grafana.stack
   datasource_uid = data.grafana_data_source.default_prometheus.id
@@ -12,3 +12,4 @@ resource "grafana_data_source_permission" "prometheus_for_viewers" {
     permission = "Query"
   }
 }
+*/
