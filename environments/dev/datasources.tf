7bb1bcb74438f79c6cb689b4d8c376e276dad874
Adam Sobkowicz <adamsobkowicz0@gmail.com>
2025-10-10 15:21:10 +0200

    4

--- environments/dev/datasources.tf
+++ environments/dev/datasources.tf
@@ -1,4 +1,5 @@
 data "grafana_data_source" "default_prometheus" {
+  provider = grafana.stack
   # Domyślna nazwa to zazwyczaj "Prometheus".
   # Jeśli jest inna, musisz ją tutaj zmienić.
   name = "grafanacloud-adamssstfdev-prom"
